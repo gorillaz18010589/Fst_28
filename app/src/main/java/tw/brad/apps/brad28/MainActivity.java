2d49038c8d68a242d9148e321369cdc56a20c766
joe <skyjoe0722@gmail.com>
2019-08-18 10:45:00 +0800

    0818

---
+++ app/src/main/java/tw/brad/apps/brad28/MainActivity.java
@@ -0,0 +1,19 @@
+package tw.brad.apps.brad28;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.Settings;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
